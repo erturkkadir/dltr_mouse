99f1cab25dc31f642da2ab54b4c24ff712000c37
Kadir <kadirerturk@gmail.com>
2018-07-28 15:03:17 -0700

    model is saved

--- ses_funcs.py
+++ ses_funcs.py
@@ -3,29 +3,53 @@ import os
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 import numpy as np
+import matplotlib.pyplot as plt
+
+import librosa.display
 
 DATA_PATH = "data/"
 
+
 def get_labels(path=DATA_PATH):
-    labels = os.listdir(path+'sesler')
+    labels = os.listdir(path)
     label_indices = np.arange(0, len(labels))
     category = to_categorical(label_indices)
-    return  labels, label_indices, category
+    return labels, label_indices, category
 
-def wav2mfcc(file_path, dim1, dim2):
+
+def wav2mfcc(file_path, dim1=20, dim2=60):
+    # Bazi orneklerde ornek sayisi 1/3 oraninda dusurulurek islem yapiliyor wave[::3]
+    # sr = 48.000/3 = 16.000, biz bu yolu tercih etmeyecegiz
     wave, sr = librosa.load(file_path, sr=None, mono=True)
-    # 83142 byte (record size) / 4 / 2 = 10392
-    mfcc = librosa.feature.mfcc(wave, n_mfcc=dim1)
-    if dim2>mfcc.shape[1]:
+    # sr = 48.000, wave ~ 48000 (1 sec record)
+    mfcc = librosa.feature.mfcc(wave, sr=sr, n_mfcc=dim1)
+    # shape of mfcc is (20, 89) : 89 is depend on record size
+    # plt.figure(1)
+    # plt.grid(True)
+    # t = [i/48000 for i in range(len(wave))]
+    # plt.subplot(211)
+    # plt.plot(t, wave)
+    # plt.subplot(212)
+    # librosa.display.specshow(mfcc, x_axis='time')
+    # plt.colorbar()
+    # plt.title('MFCC')
+    # plt.tight_layout()
+    # plt.show()
+
+    # mfcc shape is (13, X)  where x is depend on file size, it is approx 89 for 1 sec
+    if dim2 > mfcc.shape[1]:
         pad_width = dim2 - mfcc.shape[1]
-        mfcc = np.pad(mfcc, pad_width=((0,0), (0, pad_width)), mode='constant')
+        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
     else:
         mfcc = mfcc[:, :dim2]
+    # always with the dimension [20, 60] or [dim1, dim2]
+    print("MFCC Dimensions : ", file_path, mfcc.shape)
     return mfcc
+
 
 def get_train_test(dim1, dim2):
     labels, indices, hot_vector = get_labels(DATA_PATH)
-    # labels = ['asagi', 'sol', 'sag', 'yukari']
+    # labels = ['asagi', 'sag', 'sol', 'tikla', 'yukari']
     # [0, 1, 2, 3]
     # [
     # [1 0 0 0]
@@ -37,13 +61,15 @@ def get_train_test(dim1, dim2):
     x = []
     y = []
     for label in labels:
-        wav_files = ['datasets/sesler/' +label+'/' + wavfile for wavfile in os.listdir('datasets/sesler/'+label) ]
+        wav_files = ['data/' +label+'/' + wavfile for wavfile in os.listdir('data/'+label)]
         for wav_file in wav_files:
             mfcc = wav2mfcc(wav_file, dim1, dim2)
             x.append(mfcc)
-            y.append(hot_vector[indices])
+            vec = hot_vector[indices]
+            y.append(vec)
+            print("File , hot ", wav_file, vec)
         indices = indices + 1
     x = np.array(x)
     y = np.array(y)
 
-    return train_test_split(x, y, test_size=int(len(x)*0.3), shuffle=True)
+    return train_test_split(x, y, test_size=int(len(x)*0.1), shuffle=True)
--- ses_main.py
+++ ses_main.py
@@ -3,12 +3,12 @@ from ses_funcs import *
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 
-feature_dim_1 = 20
-feature_dim_2 = 40
+feature_dim_1 = 13  # in order to match Meyda
+feature_dim_2 = 60
 
 channel = 1
 epochs = 75
-num_classes = 4
+num_classes = 5     # asagi, yukari, sag, sol, tikla
 batch_size = 20
 
 x_train, x_test, y_train, y_test = get_train_test(feature_dim_1, feature_dim_2)
@@ -16,24 +16,29 @@ x_train, x_test, y_train, y_test = get_train_test(feature_dim_1, feature_dim_2)
 x_train = x_train.reshape(x_train.shape[0], feature_dim_1, feature_dim_2, channel)
 x_test = x_test.reshape(x_test.shape[0], feature_dim_1, feature_dim_2, channel)
 
+print("Data Shapes : ")
+print("x_train, y_train : ", x_train.shape, y_train.shape)
+print("x_test, y_test : ", x_test.shape, y_test.shape)
+
 
 def get_model():
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(2,2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
-    model.add(Conv2D(48, kernel_size=(2,2), activation='relu'))
-    model.add(Conv2D(120, kernel_size=(2,2), activation='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.4))
-    model.add(Dense(num_classes, activation='softmax'))
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adadelta(),
-                  metrics=['accuracy'])
-    return model
+
+    MyModel = Sequential()
+    MyModel.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
+    MyModel.add(Conv2D(48, kernel_size=(2, 2), activation='relu'))
+    MyModel.add(Conv2D(120, kernel_size=(2, 2), activation='relu'))
+    MyModel.add(MaxPool2D(pool_size=(2, 2)))
+    MyModel.add(Dropout(0.25))
+    MyModel.add(Flatten())
+    MyModel.add(Dense(128, activation='relu'))
+    MyModel.add(Dropout(0.25))
+    MyModel.add(Dense(64, activation='relu'))
+    MyModel.add(Dropout(0.4))
+    MyModel.add(Dense(num_classes, activation='softmax'))
+    MyModel.compile(loss=keras.losses.categorical_crossentropy,
+                    optimizer=keras.optimizers.Adadelta(),
+                    metrics=['accuracy'])
+    return MyModel
 
 
 def predict(filepath, model):
@@ -49,5 +54,5 @@ model = get_model()
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
 model.save('model.tf')
 
-pred = predict('datasets/sesler/yukari/yukari1.wav', model=model)
+pred = predict('data/sol/2018-07-28T00_08_59.024Z.wav', model=model)
 print(pred)
